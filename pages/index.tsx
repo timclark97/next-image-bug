7289f6289fc9a62d4406c15704b271eb66ee318c
Tim <tim@laborem.com>
2023-03-15 13:19:37 -0400

    add example that does work

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,16 @@ export default function Home() {
   return (
     <div>
       <Image src="photo.jpg" width={200} height={200} alt="Image" />
+      {/* This does work */}
+      {/* <Image
+        src="photo.jpg"
+        width={200}
+        height={200}
+        alt="Image"
+        loader={({ src, width, quality }) => {
+          return `https://via.placeholder.com/${width}`;
+        }}
+      /> */}
     </div>
   );
 }
